ca72531b85e45c9c382a6aa3d01a5b285a8dd89d
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-05-05 18:21:31 +0400

    fisrtone nah

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Evi St on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
