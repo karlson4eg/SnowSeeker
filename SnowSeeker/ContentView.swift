fe89b24b9a695e0c8cf472861494656c211cb99a
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-05-05 19:25:30 +0400

    searchable views

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+    
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
     }
 }
 
